3cd79c8524b88c4a11c6bcf5dab72fa132013256
MorganDeCadolle <entei1999@hotmail.fr>
2017-10-24 16:34:49 +0200

    Add files via upload

---
+++ config/Conf.php
@@ -0,0 +1,42 @@
+<?php
+class Conf {
+   
+  static private $databases = array(
+    // Le nom d'hote est webinfo a l'IUT
+    // ou localhost sur votre machine
+    'hostname' => 'webinfo',
+    // A l'IUT, vous avez une BDD nommee comme votre login
+    // Sur votre machine, vous devrez creer une BDD
+    'database' => 'decadollem',
+    // A l'IUT, c'est votre login
+    // Sur votre machine, vous avez surement un compte 'root'
+    'login' => 'decadollem',
+    // A l'IUT, c'est votre mdp (INE par defaut)
+    // Sur votre machine personelle, vous avez creez ce mdp a l'installation
+    'password' => '1109035104P'
+  );
+   
+  static public function getLogin() {
+    return self::$databases['login'];
+  }
+   
+  static public function getHostname() {
+    return self::$databases['hostname'];
+  }
+  
+  static public function getDatabase() {
+    return self::$databases['database'];
+  }
+  
+  static public function getPassword() {
+    return self::$databases['password'];
+  }
+  
+  static private $debug = True; 
+    
+  static public function getDebug() {
+    return self::$debug;
+  }
+}
+?>
+
