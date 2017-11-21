fa5adec326404ece290c752d1ee9f50d4969e2b7
Titdu29 <titouan.lag29@gmail.com>
2017-11-21 16:03:01 +0100

    Update CreateCommande.php

--- view/Commande/CreateCommande.php
+++ view/Commande/CreateCommande.php
@@ -10,9 +10,9 @@
 			<fieldset>
 				<legend>Mon formulaire :</legend>
 				<p>
-					<label for="login_id">id_utilisateur</label> :
+					<label for="id_utilisateur">id_utilisateur</label> :
 					<input type="text" placeholder="Ex :2" name="util" id="id_utilisateur_id" required/>
-                    <label for="mdp_id">id_adresse</label> :
+                    			<label for="id_adresse">id_adresse</label> :
 					<input type="text" placeholder="Ex : adr" name="adr" id="adr_id" required/>
 				</p>
 				<p>
