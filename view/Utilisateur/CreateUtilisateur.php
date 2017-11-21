2657181bd376418ec1ac44227ea7950d59db7a53
MorganDeCadolle <entei1999@hotmail.fr>
2017-11-21 14:03:09 +0100

    Update CreateUtilisateur.php

--- view/Utilisateur/CreateUtilisateur.php
+++ view/Utilisateur/CreateUtilisateur.php
@@ -13,7 +13,7 @@
 					<label for="login_id">Login</label> :
 					<input type="text" placeholder="Ex : xX_KikooRoxorDu39_Xx" name="login" id="login_id" required/>
                                         <label for="mdp_id">Mot de passe</label> :
-					<input type="text" placeholder="Ex : 123456" name="mdp" id="mdp_id" required/>
+					<input type="password" placeholder="Ex : 123456" name="mdp" id="mdp_id" required/>
 					<label for="nom_id">Nom</label> :
 					<input type="text" placeholder="Ex : Bob" name="nom" id="nom_id" required/>
 					<label for="prenom_id">Prenom</label> :
