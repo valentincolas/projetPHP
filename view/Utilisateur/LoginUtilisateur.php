3f8c5e87bbcd2ba25eb18b89b96f9cc1b72c1856
MorganDeCadolle <entei1999@hotmail.fr>
2017-11-21 14:17:23 +0100

    Add files via upload

--- view/Utilisateur/LoginUtilisateur.php
+++ view/Utilisateur/LoginUtilisateur.php
@@ -6,14 +6,14 @@
     </head>
    
     <body>
-		<form method="post" action="../controller/Routeur.php?action=logined">
+		<form method="post" action="../controller/Routeur.php?table=ControllerUtilisateur&action=logined">
 			<fieldset>
 				<legend>Mon formulaire :</legend>
 				<p>
 					<label for="login_id">Login</label> :
 					<input type="text" placeholder="Ex : xX_KikooRoxorDu39_Xx" name="login" id="login_id" required/>
                                         <label for="mdp_id">Mot de passe</label> :
-					<input type="password" placeholder="Ex : 123456" name="mdp" id="mdp_id" required/>
+                                        <input type="password" placeholder="Ex : 123456" name="mdp" id="mdp_id" required/>
 				</p>
 				<p>
 					<input type="submit" value="Envoyer" />
