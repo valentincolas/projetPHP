82cbc2882183c65fcad72e7bb4c3bb2f80663d97
MorganDeCadolle <entei1999@hotmail.fr>
2017-11-21 14:02:56 +0100

    Update LoginUtilisateur.php

--- view/Utilisateur/LoginUtilisateur.php
+++ view/Utilisateur/LoginUtilisateur.php
@@ -13,7 +13,7 @@
 					<label for="login_id">Login</label> :
 					<input type="text" placeholder="Ex : xX_KikooRoxorDu39_Xx" name="login" id="login_id" required/>
                                         <label for="mdp_id">Mot de passe</label> :
-					<input type="text" placeholder="Ex : 123456" name="mdp" id="mdp_id" required/>
+					<input type="password" placeholder="Ex : 123456" name="mdp" id="mdp_id" required/>
 				</p>
 				<p>
 					<input type="submit" value="Envoyer" />
