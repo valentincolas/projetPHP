1bf7f8aa923804f041d9afc0d494bcac33bc020a
valentincolas <valentin.colas.11@gmail.com>
2017-12-12 13:52:03 +0100

    Add files via upload

---
+++ controller/ControllerAddresse.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once('../lib/File.php');
+require_once (File::build_path(array('model', 'ModelAddresse.php'))); // chargement du modèle
+
+class ControllerAddresse {
+
+    public static function read() {
+        $a = ModelAddresse::getAddresseById($_GET['id']);
+        if ($a == false) {
+            $controller = "Addresse";
+            $view = 'ErreurAddresseId';
+            $pagetitle = 'ERREUR';
+            require (File::build_path(array('view', 'view.php')));
+        } else {
+            $controller = "Addresse";
+            $view = 'AddresseDetail';
+            $pagetitle = 'Detail Adresse';
+            require (File::build_path(array('view', 'view.php')));
+        }
+    }
+
+    public static function readAll() {
+        $tab_a = ModelAddresse::getAllAddresse(); //appel au modèle pour gerer la BD
+        $controller = "Addresse";
+        $view = 'ListeAddresse';
+        $pagetitle = 'Liste Adresse';
+        require (File::build_path(array('view', 'view.php')));
+    }
+
+    public static function create() {
+        $controller = "Addresse";
+        $view = 'CreateAddresse';
+        $pagetitle = 'Creation Adresse';
+        require (File::build_path(array('view', 'view.php')));
+    }
+
+    public static function created() {
+        $i = 0;
+        $tab_a = ModelAddresse::getAllAddresse();
+        foreach ($tab_a as $a) {
+            if ($a->getVille() == $_POST['ville'] && $a->getCodePostal() == $_POST['codepostal'] && $a->getRue() == $_POST['rue'] && $a->getPays() == $_POST['pays']) {
+                $i = 1;
+            }
+        }
+        if ($i == 1) {
+            $controller = "Addresse";
+            $view = 'ErreurAddresseCreated';
+            $pagetitle = 'ERREUR';
+            require (File::build_path(array('view', 'view.php')));
+        } else {
+            $a = new ModelAddresse($_POST['rue'], $_POST['ville'], $_POST['codepostal'], $_POST['pays']);
+            $a->save();
+        }
+    }
+
+    public static function deleted() {
+        $a = ModelAddresse::getAddresseById($_GET['id']);
+        if ($a == false) {
+            $controller = "Addresse";
+            $view = 'ErreurAddresseId.php';
+            $pagetitle = 'Erreur Adresse';
+            require (File::build_path(array('view', 'view.php')));
+        } else {
+            $a->delete();
+        }
+    }
+
+}
+?>
+
--- controller/ControllerProduit.php
+++ controller/ControllerProduit.php
@@ -1,45 +1,71 @@
 <?php
+
 require_once('../lib/File.php');
-require_once (File::build_path(array('model','ModelProduit.php'))); // chargement du modèle
+require_once (File::build_path(array('model', 'ModelProduit.php'))); // chargement du modèle
 
 class ControllerProduit {
-    
-    public static function read(){
+
+    public static function read() {
         $p = ModelProduit::getProduitById($_GET['id']);
-        if($p == false){
-            require (File::build_path(array('view','Produit','ErreurProduitId.php')));
+        if ($p == false) {
+            $controller = "Produit";
+            $view = 'ErreurProduitID';
+            $pagetitle = 'ERREUR';
+            require (File::build_path(array('view', 'view.php')));
         } else {
-            require (File::build_path(array('view','Produit','ProduitDetail.php')));
-        } 
+            $controller = "Produit";
+            $view = 'ProduitDetail';
+            $pagetitle = 'Detail Produit';
+            require (File::build_path(array('view', 'view.php')));
+        }
     }
-    
-    public static function create(){
-        require (File::build_path(array('view','Produit','CreateProduit.php')));
+
+    public static function readAll() {
+        $tab_p = ModelProduit::getAllProduit(); //appel au modèle pour gerer la BD
+        $controller = "Produit";
+        $view = 'ListeProduit';
+        $pagetitle = 'Liste Produit';
+        require (File::build_path(array('view', 'view.php')));
     }
-    
-    public static function created(){
+
+    public static function create() {
+        $controller = "Produit";
+        $view = 'CreateProduit';
+        $pagetitle = 'Creation Produit';
+        require (File::build_path(array('view', 'view.php')));
+    }
+
+    public static function created() {
         $i = 0;
         $tab_p = ModelProduit::getAllProduit();
-        foreach ($tab_p as $p){
-            if($p->getNom() == $_POST['nom']){$i = 1;}
+        foreach ($tab_p as $p) {
+            if ($p->getNom() == $_POST['nom']) {
+                $i = 1;
+            }
         }
-        if($i == 1){
-            require (File::build_path(array('view','Produit','ErreurProduitCreated.php')));
+        if ($i == 1) {
+            $controller = "Produit";
+            $view = 'ErreurProduitCreated';
+            $pagetitle = 'ERREUR';
+            require (File::build_path(array('view', 'view.php')));
         } else {
             $p = new ModelProduit($_POST['nom'], $_POST['prix'], $_POST['stock'], $_POST['lien'], $_POST['description']);
             $p->save();
         }
     }
-    
-    
-    public static function deleted(){
+
+    public static function deleted() {
         $p = ModelProduit::getProduitById($_GET['id']);
-        if($p == false){
-            require (File::build_path(array('view','Produit','ErreurProduitId.php')));
+        if ($p == false) {
+            $controller = "Produit";
+            $view = 'ErreurProduitId';
+            $pagetitle = 'ERREUR';
+            require (File::build_path(array('view', 'view.php')));
         } else {
             $p->delete();
         }
     }
+
 }
 ?>
 
