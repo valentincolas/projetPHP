1b028f8176099dcf7a1768bf5c78daba81e14acf
MorganDeCadolle <entei1999@hotmail.fr>
2017-12-19 13:57:33 +0100

    Update Routeur.php

--- controller/Routeur.php
+++ controller/Routeur.php
@@ -1,11 +1,16 @@
 <?php
 require_once 'ControllerUtilisateur.php';
+require_once 'ControllerAddresse.php';
+require_once 'ControllerCommande.php';
+require_once 'ControllerProduit.php';
+require_once 'ControllerPanier.php';
+require_once 'ControllerContenuCommande';
 if (isset($_GET["table"]) && isset($_GET["action"])){
     $table = $_GET["table"];
     $action = $_GET["action"];
     $table::$action(); 
 } else {
-    echo "Cette page n'existe pas";
+    ControllerProduit::readAll();
 }
 
 ?>
