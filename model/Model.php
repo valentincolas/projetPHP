f01c8862aedf0429e665f47566f68fb1ab9c80dd
Titdu29 <titouan.lag29@gmail.com>
2017-12-19 12:44:25 +0100

    Update Model.php

--- model/Model.php
+++ model/Model.php
@@ -1,6 +1,5 @@
 <?php
 require_once '../config/Conf.php';
-
 class Model {
    
   static public $pdo;
@@ -23,7 +22,62 @@ class Model {
         die();
       }
   }
-}
 
+   
+   public static function selectAll(){
+        $table_name = static::$object;
+        $class_name='Model'.ucfirst($table_name);
+        $table_name = "P_".$table_name;
+        $rep = Model::$pdo->query("SELECT * FROM $table_name");
+        $rep->setFetchMode(PDO::FETCH_CLASS, $class_name );
+        $tab_voit = $rep->fetchAll();
+        return $tab_voit;
+   }
+   
+   public static function select($primary_value){
+        $table_name = static::$object;
+        $class_name='Model'.ucfirst($table_name);
+        $table_name = "P_".$table_name;
+        $primary_key= static::$primary;
+        
+        $sql = "SELECT * from $table_name WHERE $primary_key=:nom_tag";
+        // Préparation de la requête
+        $req_prep = Model::$pdo->prepare($sql);
+        $values = array(
+            "nom_tag" => $primary_value,
+            //nomdutag => valeur, ...
+        );
+        // On donne les valeurs et on exécute la requête	 
+        $req_prep->execute($values);
+
+        // On récupère les résultats comme précédemment
+        $req_prep->setFetchMode(PDO::FETCH_CLASS, $class_name);
+        $tab = $req_prep->fetchAll();
+        // Attention, si il n'y a pas de résultats, on renvoie false
+        if (empty($tab)) return false;
+        return $tab[0];
+   }
+   
+   public static function delete($primary_value){
+       
+        $table_name = static::$object;
+        $class_name='Model'.ucfirst($table_name);
+        $table_name = "P_".$table_name;
+        $primary_key= static::$primary;
+            
+       
+        $sql = "DELETE FROM $table_name WHERE $primary_key=:id";
+        $req_prep = Model::$pdo->prepare($sql);
+        $values = array(
+            "id" => $primary_value,
+        );
+            
+        $req_prep->execute($values);
+   }
+   
+   public static function update($primary_value){
+       
+   }
+   
+}
 Model::Init();
-?>
